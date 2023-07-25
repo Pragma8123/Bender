5f301866cfa354a5646bca0f5370be4a7008e888
Pragma8123 <max@machon.family>
2023-07-25 11:43:20 -0400

    v4.0.1

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -33,7 +33,7 @@
 /**
  * Bender release version identifier
 */
-#define BENDER_BUILD_VERSION "v4.0.0"
+#define BENDER_BUILD_VERSION "v4.0.1"
 
 /**
  * Verbose version identifier which should contain a reference to the location
