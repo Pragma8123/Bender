0b32802c0805512a1ec482d6c5aa0dab16b28cc5
Pragma8123 <max@machon.family>
2023-07-25 18:53:26 -0400

    fix: correct nozzle-to-probe z offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1553,7 +1553,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -32, -40, -2.52 }
+#define NOZZLE_TO_PROBE_OFFSET { -32, -40, -2.70 }
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
