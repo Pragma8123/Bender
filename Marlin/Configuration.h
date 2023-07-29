c6115d82290a54e11ebeb185786f9b060b05a69e
Pragma8123 <max@machon.family>
2023-07-29 12:58:49 -0400

    fix: further refine z offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1553,7 +1553,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -31.8, -40.5, -2.73 }
+#define NOZZLE_TO_PROBE_OFFSET { -31.8, -40.5, -2.76 }
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
